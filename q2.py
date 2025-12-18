d2ab9bd042fea220791a72678d0755cdafd1338f
mohanjawahar <rkvfvjt9gv@privaterelay.appleid.com>
2025-12-19 03:10:39 +0800

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,1 +1,9 @@
+def find_and_replace(val, find_val, replace_val):
+    a = list(map(lambda x: replace_val if x == find_val else x, val))
+    return a
 
+
+val1 = find_and_replace([1,2,3,4,2,2],2,5)
+print(val1)
+val2 = find_and_replace(['apple','banana','apple'],'apple','orange')
+print(val2)
