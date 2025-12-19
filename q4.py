509cf5cbec20accf4a4e8b3e3121be5f10348e9f
mohanjawahar <rkvfvjt9gv@privaterelay.appleid.com>
2025-12-19 20:44:40 +0800

    Update q4.py

--- q4.py
+++ q4.py
@@ -3,7 +3,10 @@ def string_reverse(s):
   rs = s[::-1]
   return rs
 
+# Main Calling function - Scenario 1
 rs1 = string_reverse("Hello World")
 print(f'Reversed String value: {rs1}')
+
+# Main Calling function - Scenario 2
 rs2 = string_reverse("Python")
 print(f'Reversed String value: {rs2}')
